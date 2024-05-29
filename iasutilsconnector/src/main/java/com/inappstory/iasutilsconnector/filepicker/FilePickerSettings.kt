ea1261d84702e0859518c1e90d9a85a6b3314d32
paperrose <paperrose@mail.ru>
2024-05-29 14:15:57 +0300

    - File picker config and settings

---
+++ iasutilsconnector/src/main/java/com/inappstory/iasutilsconnector/filepicker/FilePickerSettings.kt
@@ -0,0 +1,9 @@
+package com.inappstory.iasutilsconnector.filepicker
+
+class FilePickerSettings {
+    var id: String? = null
+    var cb: String? = ""
+    var accept: String = ""
+    var multiple: Boolean? = null
+    var config: FilePickerConfig? = null
+}
